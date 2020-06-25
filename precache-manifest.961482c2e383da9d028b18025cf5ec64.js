20f00a0eaced7059bc06596ced3d9223db927b3c
robytanama <tanamaroby@gmail.com>
2020-06-25 16:02:54 +0800

    Updates

--- precache-manifest.961482c2e383da9d028b18025cf5ec64.js
+++ precache-manifest.961482c2e383da9d028b18025cf5ec64.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9014985f6e080e4a3a0c870010fd033b",
+    "revision": "91c609444536d9b9b7c274a8f43ba9f7",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "0faac006f2106baa74f9",
-    "url": "/portfolio/static/css/main.7928b52a.chunk.css"
+    "revision": "29aa94c233831b176a0b",
+    "url": "/portfolio/static/css/main.8bb4873c.chunk.css"
   },
   {
     "revision": "bf79e0fc48d59fd198f7",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.4c736102.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0faac006f2106baa74f9",
-    "url": "/portfolio/static/js/main.84bf6e15.chunk.js"
+    "revision": "29aa94c233831b176a0b",
+    "url": "/portfolio/static/js/main.b6a82583.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f1c819775576a9266907dce0334186c7.js"
+  "/portfolio/precache-manifest.961482c2e383da9d028b18025cf5ec64.js"
 );
 
 self.addEventListener('message', (event) => {
