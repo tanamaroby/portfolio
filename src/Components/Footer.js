584471bc6a6644f961c893d9f093b4a30deca20d
robytanama <tanamaroby@gmail.com>
2020-06-25 19:40:08 +0800

    Finish header section prototype

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return(
         <div className="footer">
-            <footer>This website is currently in development!</footer>
+            <footer>This website is currently under development.</footer>
         </div>
     )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
-import profileImage from './Resources/Images/ProfilePic.jpg'
+import profileImage from './Resources/Images/ProfilePic.jpg';
+import telegramLogo from './Resources/Images/Telegram.png';
+import githubLogo from './Resources/Images/Github.png';
+import instagramLogo from './Resources/Images/Instagram.png';
+import linkedInLogo from './Resources/Images/LinkedIn.jpg';
 
 function Header() {
     var date = new Date();
@@ -15,14 +19,32 @@ function Header() {
     }
 
     return(
-        <div className="header">
-            <img className="image" src={profileImage} alt="Profile"/>
-            <div className="greeting">
-                <p>Good {timeOfDay}!</p>
-                <span>My name is </span>
-                <span className="name">Roby Tanama</span>
+        <div>
+            <div className="header">
+                <img className="image" src={profileImage} alt="Profile"/>
+                <div className="greeting">
+                    <p>Good {timeOfDay}!</p>
+                    <span>I'm </span>
+                    <span className="name">Roby Tanama</span>
+                </div>
             </div>
-            
+            <div className="social">
+                <h1>@robytanama</h1>
+                <div className="logo">
+                    <a href="https://t.me/robytanama">
+                        <img className="image" src={telegramLogo} alt="Telegram" />
+                    </a>
+                    <a href="https://github.com/robytanama">
+                        <img className="image" src={githubLogo} alt="Github" />
+                    </a>
+                    <a href="https://instagram.com/robytanama">
+                        <img className="image" src={instagramLogo} alt="Instagram" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/robytanama/">
+                        <img className="image" src={linkedInLogo} alt="LinkedIn" />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
