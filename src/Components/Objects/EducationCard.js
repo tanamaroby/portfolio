76fa3992cd1262961ec089aa1c8b88f6bfcad411
robytanama <tanamaroby@gmail.com>
2020-06-26 16:38:25 +0800

    Made css fields static to be handled for mobile later

--- src/Components/Objects/EducationCard.js
+++ src/Components/Objects/EducationCard.js
@@ -1,31 +1,12 @@
 import React from "react";
+import './EducationCard.css';
 
 function EducationCard(props) {
 
-    // Card style
-    const cardStyle = {
-        display: 'flex',
-        alignItems: 'center'
-    };
-
-    // Img style
-    const imgStyle = {
-        borderRadius: '3vw',
-        width: '20%',
-        height: '20%',
-        border: '0.8vw solid #1D3557',
-        margin: '5%'
-    };
-
-    // Text style
-    const textStyle = {
-        fontSize: 'calc(60% + 0.5vw)'
-    }
-
     return(
-        <div style={cardStyle}>
-            <img style={imgStyle} src={props.imgUrl} />
-            <div style={textStyle}>
+        <div className='schoolcard'>
+            <img className='schoolimage' src={props.imgUrl} alt='Logo' />
+            <div className='schooltext'>
                 <h2>{props.name}</h2>
                 <p>{props.description}</p>
             </div>
